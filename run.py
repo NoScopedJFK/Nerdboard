e492064ac32c093a8d6ac576225da70c31589759
Karsenity <48289014+Karsenity@users.noreply.github.com>
2020-04-11 01:57:20 -0400

    Update run.py

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-from src.app import app
+app = Flask(name)
 
-app.run(debug=True)
+# Make the WSGI interface available at the top level so wfastcgi can get it.
+wsgi_app = app.wsgi_app
+
+#   from src.app import app
+
+@app.route('/')
+def home():
+    """Renders a Home page."""
+    return "Hello"
+ 
+if __name__=="__main__":
+  app.run(debug=True)
